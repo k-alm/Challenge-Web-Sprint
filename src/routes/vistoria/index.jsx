7a73be0e8e86d37f7da10fab00dd1fdbeeb97f58
biasvestka <biasvestka@gmail.com>
2023-09-12 18:05:14 -0300

    página de vistoria feita

--- src/routes/vistoria/index.jsx
+++ src/routes/vistoria/index.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Vistoria(){
     return(
-
+        <main>
+            <img src="src/assets/img-porto-logo.png" alt="Porto Seguro's logo with background"/>
+            <form className="container">
+                <h1 className="container__title">Escaneie o QR Code para iniciar a vistoria</h1>
+                <p>QR Code Aqui</p>
+                <p>O usuário será redirecionado para a página mobile da vistoria ao escanear</p>      
+                <Link className="link-to campos__buttons__layout" to="/vmobile">Continuar</Link>
+            </form>
+        </main>
     );
 }
