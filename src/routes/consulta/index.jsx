32ee055c6df89f68fb1af41a476b86c6f29f4c3c
nichol6s <nicholas.jake15@gmail.com>
2023-09-12 16:49:28 -0300

    página de consulta finalizada

--- src/routes/consulta/index.jsx
+++ src/routes/consulta/index.jsx
@@ -1,5 +1,19 @@
+import Inputs from "../../components/Inputs/inputs";
+import { Link } from "react-router-dom";
+
 export default function Consulta(){
     return(
-
+        <main>
+            <img src="src/assets/img-porto-logo.png" alt="Porto Seguro's logo with background"/>
+            <form className="container">
+                <h1>Consultar Bike</h1>
+                <h1 className="container__title">Insira o número de série da bike</h1>
+                <div className="campos">
+                    <Inputs class="campos__texto" placeholder="Digite o número de série" label="Número de série" type="text"/>
+                </div>
+                <button className="link-to-consulta campos__buttons__layout" to="/vistoria">Iniciar Vistoria</button>              
+                <Link className="link-to-consulta campos__buttons__layout" to="/">Voltar</Link>
+            </form>
+        </main>
     );
 }
