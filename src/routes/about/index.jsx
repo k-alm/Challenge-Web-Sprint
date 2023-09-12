8b53cb5a983166dca924e5aaf23aa8517939effc
nichol6s <nicholas.jake15@gmail.com>
2023-09-12 16:46:32 -0300

    Fazendo a parte de sobre nós

--- src/routes/about/index.jsx
+++ src/routes/about/index.jsx
@@ -1,5 +1,14 @@
 export default function About(){
     return(
-
+        <main class="content">
+            
+            <section class="container">
+                <h1 class="container__title">Sobre Nós:</h1><br/>
+                <p class="container__title">Kauan de Almeida - RM: 552075</p><br/>
+                <p class="container__title">Nicholas Santos - RM: 551809</p><br/>
+                <p class="container__title">Murillo Scaff - RM: 551641</p><br/>
+                <p class="container__title">Beatriz Svestka Santos - RM: 551534</p>
+            </section>
+        </main>
     );
 }
