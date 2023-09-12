f76c9b9062692c33cc338c34c7c6633c47795eb1
k-alm <kauan.alm.almeida@gmail.com>
2023-09-12 14:37:26 -0300

    export default das routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error404 from './routes/error/index.jsx'
+import VistoriaMobile from './routes/vistoria-mobile/index.jsx'
+import About from './routes/about/index.jsx'
+import Content from './routes/Content/index.jsx'
+import Consulta from './routes/consulta/index.jsx'
+import Registro from './routes/registro/index.jsx'
+import Vistoria from './routes/vistoria/index.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    errorElement: <Error404/>,
+    children:[
+      {
+        path:"/",
+        element: <Content/>
+      },
+      {
+        path:"/consulta",
+        element: <Consulta/>
+      },
+      {
+        path:"/vistoria",
+        element: <Vistoria/>
+      },
+      {
+        path:"/vmobile",
+        element: <VistoriaMobile/>
+      },
+      {
+        path:"/registro",
+        element: <Registro/>
+      },
+      {
+        path:"/about",
+        element: <About/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
