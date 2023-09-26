47d3582d6b19dfee615ebaa4b067d5ba7767fcc9
k-alm <kauan.alm.almeida@gmail.com>
2023-09-26 09:17:42 -0300

    arquivos desnecessários removidos

--- src/routes/about/index.jsx
+++ src/routes/about/index.jsx
@@ -1,14 +1,14 @@
 export default function About(){
     return(
-        <main class="content">
+        <main className="content">
             <img src="src/assets/img-porto-logo.png" alt="Porto Seguro's logo with background"/>
-            <section class="container">
-                <h1 class="container__title">Sobre Nós:</h1><br/>
-                <p class="container__title">Kauan de Almeida - RM: 552075</p><br/>
-                <p class="container__title">Nicholas Santos - RM: 551809</p><br/>
-                <p class="container__title">Murillo Scaff - RM: 551641</p><br/>
-                <p class="container__title">Beatriz Svestka Santos - RM: 551534</p>
-                <a class="campos__buttons__layout" target="blank" href="https://github.com/k-alm/Challenge-Web-Sprint.git">Repositório do github</a>
+            <section className="container">
+                <h1 className="container__title">Sobre Nós:</h1><br/>
+                <p className="container__title">Kauan de Almeida - RM: 552075</p><br/>
+                <p className="container__title">Nicholas Santos - RM: 551809</p><br/>
+                <p className="container__title">Murillo Scaff - RM: 551641</p><br/>
+                <p className="container__title">Beatriz Svestka Santos - RM: 551534</p>
+                <a className="campos__buttons__layout" target="blank" href="https://github.com/k-alm/Challenge-Web-Sprint.git">Repositório do github</a>
             </section>
         </main>
     );
